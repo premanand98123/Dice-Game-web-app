2c18d168f80c05bcba82f7338fa44411557bc806
premanand98123 <77653316+premanand98123@users.noreply.github.com>
2022-06-28 22:01:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let images = ["dice-01.svg",
+"dice-02.svg",
+"dice-03.svg",
+"dice-04.svg",
+"dice-05.svg",
+"dice-06.svg"];
+let dice = document.querySelectorAll("img");
+
+function roll(){
+    dice.forEach(function(die){
+        die.classList.add("shake");
+    });
+    setTimeout(function(){
+        dice.forEach(function(die){
+            die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random()*6);
+        let dieTwoValue = Math.floor(Math.random()*6);
+        console.log(dieOneValue,dieTwoValue);
+        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
+        document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
+
+        if(dieOneValue == dieTwoValue)
+        {
+          document.querySelector("#total").innerHTML = " Its a Draw!! ";
+        }
+
+        else if (dieOneValue < dieTwoValue) {
+          document.querySelector("#total").innerHTML = " Player 2 wins!! ";
+        }
+
+        else{
+            document.querySelector("#total").innerHTML = " Player 1 wins!! ";
+        }
+
+    },
+    1000
+    );
+}
+roll();
